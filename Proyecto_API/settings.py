5c04ac7786ebeb191424aaadc94267476be9982b
Juan <juanorarola2@gmail.com>
2022-06-02 03:37:41 -0400

    make it better

--- Proyecto_API/settings.py
+++ Proyecto_API/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nejojgcipqf&zl#e5hgq_=db3wjm4nvwsq5q811l=odlllu=m)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
